fcfafecd2b2b9263508bb6840e27e469fe201d97
Eitan Suez <eitan.suez@gmail.com>
2017-07-06 09:43:46 -0500

    verified version 0.7 of terraforming gcp works, updated docs and scripts to match

--- packer/provision.sh
+++ packer/provision.sh
@@ -2,9 +2,9 @@
 sudo apt install -y unzip
 wget https://releases.hashicorp.com/terraform/0.8.4/terraform_0.8.4_linux_amd64.zip
 unzip terraform_0.8.4_linux_amd64.zip -d bin
-wget https://github.com/pivotal-cf/terraforming-gcp/archive/v0.6.0.zip
-ln -s ~/terraforming-gcp-0.6.0 terraforming-gcp
-unzip v0.6.0.zip
+wget https://github.com/pivotal-cf/terraforming-gcp/archive/v0.7.0.zip
+ln -s ~/terraforming-gcp-0.7.0 terraforming-gcp
+unzip v0.7.0.zip
 mkdir downloads
 mv *.zip downloads
 
